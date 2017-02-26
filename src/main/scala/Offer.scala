7202e95508ea3d04147f3c498a0c31d007bc969f
Misterturtle <Ryan@conaway58.com>
2017-02-26 10:14:29 -0500

    Test added: An offer should require a price to be created. Tests refactored to get to green:  -An offer should have a positive price  -An offer should have a last modified date

--- src/main/scala/Offer.scala
+++ src/main/scala/Offer.scala
@@ -3,9 +3,9 @@ import java.util.{Calendar}
 /**
   * Created by Harambe on 2/26/2017.
   */
-class Offer {
+class Offer(startingPrice: Double) {
 
-  val currentPrice:Double = 0.001
+  val currentPrice:Double = startingPrice
   val cal = Calendar.getInstance
   cal.set(2017, 2, 26)
   val lastModified = cal.getTime
--- src/test/scala-2.12/OfferTests.scala
+++ src/test/scala-2.12/OfferTests.scala
@@ -8,13 +8,26 @@ import org.scalatest.{Matchers, FlatSpec}
 class OfferTests extends FlatSpec with Matchers {
 
   "An offer" should "have a positive price" in {
-    val offer = new Offer()
+    val offer = new Offer(0.001)
     offer.currentPrice should be > 0.00
   }
 
   it should "have a last modified date" in {
-    val offer = new Offer()
+    val offer = new Offer(0.001)
     offer.lastModified shouldBe a [Date]
   }
+
+  it should "require a price to be created" in {
+    val price = 9.5
+    val offer = new Offer(price)
+    offer.currentPrice shouldEqual 9.5
+  }
+
+//  it should "have a method that changes the price" in {
+//    val offer = new Offer()
+//    val updatedOffer = offer.ChangePrice(5.05)
+//
+//    updatedOffer.currentPrice = 5.05
+//  }
 
 }
